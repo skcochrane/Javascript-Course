d4bd6984547b4b94d5774aeb3d980fa5b105749d
Sarah Cochrane <sarah.kristine.cochrane@gmail.com>
2022-10-09 16:23:04 -0700

    final modal windows

--- 06-Modal-Window/script.js
+++ 06-Modal-Window/script.js
@@ -1,1 +1,29 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModalBtn = document.querySelector(".close-modal");
+const showModalBtns = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < showModalBtns.length; i++) {
+  showModalBtns[i].addEventListener("click", openModal);
+}
+
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
