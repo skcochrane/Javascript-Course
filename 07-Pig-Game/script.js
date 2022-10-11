8f4bcb76115dfc45c48615eec10d6bf2cb7f6fe7
Sarah Cochrane <sarah.kristine.cochrane@gmail.com>
2022-10-10 22:39:23 -0700

    pig game js-update switchPlayer and reset

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -11,31 +11,76 @@ let player2Current = document.querySelector("#current--1");
 
 let diceImg = document.querySelector(".dice");
 const newGame = document.querySelector(".btn--new");
-const rollDice = document.querySelector("btn--roll");
+const rollDice = document.querySelector(".btn--roll");
 const hold = document.querySelector(".btn--hold");
 
+let diceNumber = Math.trunc(Math.random() * 6) + 1;
+let activePlayer = document
+  .querySelector(".player")
+  .classList.contains("player--active");
+
+player1Score.textContent = 0;
+player2Score.textContent = 0;
 //Functions
 
-// Roll Dice
-let diceNumber = Math.trunc(Math.random() * 6) + 1;
-if (diceNumber === 1) {
-  diceImg.src = "dice-1.png";
-} else if (diceNumber === 2) {
-  diceImg.src = "dice-2.png";
-} else if (diceNumber === 3) {
-  diceImg.src = "dice-3.png";
-} else if (diceNumber === 4) {
-  diceImg.src = "dice-4.png";
-} else if (diceNumber === 5) {
-  diceImg.src = "dice-5.png";
-} else if (diceNumber === 6) {
-  diceImg.src = "dice-6.png";
-}
+const switchPlayer = function () {
+  if (player1Sect.classList.contains("player--active")) {
+    player1Sect.classList.remove("player--active");
+    player2Sect.classList.add("player--active");
+  } else if (player2Sect.classList.contains("player--active")) {
+    player2Sect.classList.remove("player--active");
+    player1Sect.classList.add("player--active");
+  }
+};
 
-//Current Score
+// Roll Dice
+rollDice.addEventListener("click", function () {
+  diceNumber = Math.trunc(Math.random() * 6) + 1;
+  switch (diceNumber) {
+    case 1:
+      diceImg.src = "dice-1.png";
+      break;
+    case 2:
+      diceImg.src = "dice-2.png";
+      break;
+    case 3:
+      diceImg.src = "dice-3.png";
+      break;
+    case 4:
+      diceImg.src = "dice-4.png";
+      break;
+    case 5:
+      diceImg.src = "dice-5.png";
+      break;
+    case 6:
+      diceImg.src = "dice-6.png";
+      break;
+  }
+});
+
+//Current Score ---- THIS DOESN"T DO ANYTHING WITH diceNumber! new diceNumber is stuck inside rollDice function, it doesn't follow outside the function to allow interaction with current score...
+if (diceNumber === 1) {
+  switchPlayer;
+} else {
+  if (player1Sect.classList.contains("player--active")) {
+    player1Current.textContent = player1Current + diceNumber;
+  } else if (player2Sect.classList.contains("player--active")) {
+    player2Current.textContent = player2Current + diceNumber;
+  }
+}
 
 //Hold Score
 
-//Switch Players
-
 //New Game
+const resetGame = function () {
+  player1Score.textContent = 0;
+  player2Score.textContent = 0;
+  player1Current.textContent = 0;
+  player2Current.textContent = 0;
+  if (player2Sect.classList.contains("player--active")) {
+    player2Sect.classList.remove("player--active");
+    player1Sect.classList.add("player--active");
+  }
+};
+
+newGame.addEventListener("click", resetGame);
